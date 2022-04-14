f7262632830df3f172ce16bc55d521d53fbe8c36
abhibhatt48 <47849440+abhibhatt48@users.noreply.github.com>
2022-04-14 12:31:09 +0530

    assignment two push

---
+++ keyvalueIoT/Data/IMatchRepository.cs
@@ -0,0 +1,22 @@
+﻿using keyvalueIoT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace keyvalueIoT.Data
+{
+    public interface IMatchRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete(Keyvalue keyvalue);
+        void Update(string key, string value);
+
+        Task<bool> SaveChanges();
+
+        //Get method
+        public bool Exist(string key);
+
+        Keyvalue GetMatchbykey(string key);
+    }
+}
