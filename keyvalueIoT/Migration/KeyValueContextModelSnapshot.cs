caa1161f84f01bca9c9275974c0737e62fc1ea25
abhibhatt48 <47849440+abhibhatt48@users.noreply.github.com>
2022-04-18 11:03:17 +0530

    bug fix

--- keyvalueIoT/Migration/KeyValueContextModelSnapshot.cs
+++ keyvalueIoT/Migration/KeyValueContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace keyvalueIoT.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.4")
+                .HasAnnotation("ProductVersion", "3.1.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1, 1);
